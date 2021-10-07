c4b55bdb63ec1ecbb484ca46fcbb8371ce506076
one-for-ole <maiku19950522@gmail.com>
2021-10-07 20:21:12 +0900

    マいーグレーションの追加

---
+++ database/migrations/2021_10_02_042851_create_user_inf_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_inf', function (Blueprint $table) {
+            $table->integer('id', 11)->autoIncrement();
+            $table->string('company_name', 255);
+            $table->string('user_name', 255);
+            $table->string('mail', 255)->unique();
+            $table->string('password', 255);
+            $table->timestamps();
+            $table->tinyInteger('deleted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_inf');
+    }
+}
